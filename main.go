3586923160a605671c32d1b724a857a12a375960
Lennart Querter <lennart.querter@gmail.com>
2019-11-08 07:17:29 +0100

    create folders in go, functions in asm

--- main.go
+++ main.go
@@ -4,10 +4,20 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 	"os/exec"
 )
 
 func main() {
+
+	if _, err := os.Stat("./assemble"); os.IsNotExist(err) {
+		os.Mkdir("./assemble", os.ModePerm)
+	}
+
+	if _, err := os.Stat("./bin"); os.IsNotExist(err) {
+		os.Mkdir("./bin", os.ModePerm)
+	}
+
 	fmt.Printf("Assembling program\n")
 	err := assemble()
 	if err != nil {
@@ -58,5 +68,25 @@ func link() error {
 
 func assemble() error {
 	cmd := exec.Command("nasm", "-f", "macho64", "src/main.asm", "-o", "./assemble/main.o")
-	return cmd.Run()
+
+	stderr, err := cmd.StderrPipe()
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	slurpOut, _ := ioutil.ReadAll(stdout)
+	fmt.Printf("out: %s\n", slurpOut)
+
+	slurpErr, _ := ioutil.ReadAll(stderr)
+	fmt.Printf("err: %s\n", slurpErr)
+
+	if err := cmd.Wait(); err != nil {
+		log.Fatal(err)
+	}
+	return err
 }
--- src/main.asm
+++ src/main.asm
@@ -1,49 +1,65 @@
 global start
 
+; Create variables to play with
+section .data
+    x:      equ     100
+    y:      equ     50
+    total:  equ     50
+
+    correct_label:    db      "correct!", 10
+    .len:       equ     $ - correct_label
+
+    wrong_label:      db      "its wrong!", 10
+    .len:       equ     $ - wrong_label
+
 section .text
 
+; start the function by jumping directly to the sum label
 start:
     jmp .sum
 
+; sum function where we move x to the RAX register and y into the RBX register
 .sum:
     mov     rax, x
     mov     rbx, y
 
+    ; add rax to rbx, the result is stored in rax
     add     rax, rbx
 
+    ; compare rax to the total
     cmp     rax, total
+    ; if not equal jump to label wrong reguls
     jne     .wrong
-    jmp     .right
-
-.right:
-    mov     rax, 0x2000004      ; write
-    mov     rdi, 1              ; stdout
-    mov     rsi, correct
-    mov     rdx, correct.len
-    syscall
+    ; else call the right result
+    call     right
     jmp     .exit
 
 .wrong:
-    mov     rax, 0x2000004      ; write
-    mov     rdi, 1              ; stdout
-    mov     rsi, wrong
-    mov     rdx, wrong.len
-    syscall
-    jmp     .exit
+    call wrong
+    jmp .exit
 
 .exit:
       mov     rax, 0x2000001 ; exit
       mov     rdi, 0
       syscall
 
+; print out wrong label
+wrong:
+    mov     rsi, wrong_label
+    mov     rdx, wrong_label.len
+    call    print
+    ret
 
-section .data
-    x:      equ     100
-    y:      equ     50
-    total:  equ     150
+; print out the correct label
+right:
+    mov     rsi, correct_label
+    mov     rdx, correct_label.len
+    call    print
+    ret
 
-    correct:    db      "correct!", 10
-    .len:       equ     $ - correct
-
-    wrong:      db      "its wrong!", 10
-    .len:       equ     $ - wrong
+; print function
+print:
+    mov     rax, 0x2000004      ; write
+    mov     rdi, 1              ; stdout
+    syscall
+    ret
